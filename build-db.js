befe8e65afaafc8733ce2f9499b4b90fd36edb0f
Hunter Phillips <hkphillips42@gmail.com>
2018-04-19 14:34:26 -0500

    export generateHash, hash user data passwords in build-db

--- build-db.js
+++ build-db.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let models = require('./models');
+// let models = require('./models');
 
 let { users } = require('./seeders/data/users');
 let { products } = require('./seeders/data/products');
@@ -8,6 +8,13 @@ let { paymentTypes } = require('./seeders/data/paymentTypes');
 let { orders } = require('./seeders/data/orders');
 let { orderProducts } = require('./seeders/data/orderProducts');
 let { productTypes } = require('./seeders/data/productTypes');
+
+const { generateHash } = require('./config/passport-strat.js');
+
+// hash user data passwords
+users.forEach(user => {
+  user["password"] = generateHash(user["password"])
+})
 
 models.sequelize.sync({ force: true })
 .then(() => {
@@ -31,7 +38,6 @@ models.sequelize.sync({ force: true })
 .then(() => {
   process.exit();
 });
-
 
 // Proof that the bulkCreate succeeded =>
 // --------------------------------------
