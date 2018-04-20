265c6f0e84370c4335842801a7a7d27806b67cf2
Tim A. Conner <timaconner1@gmail.com>
2018-04-20 15:30:04 -0500

    Remove console logs and use user id to select products.

--- controllers/inventoryCtrl.js
+++ controllers/inventoryCtrl.js
@@ -2,14 +2,8 @@
 
 module.exports.renderInventory = (req, res) => {
   const { Product, OrderProduct } = req.app.get('models');
-
-  // TODO: Count products sold to find how many products left and how many sold
-
-  // req.userId
-
-  // TODO: Replace user id with my user name
   Product.findAll({
-    where: { user_id: 1 },
+    where: { user_id: req.user.id },
     include: [
       {
         model: OrderProduct
@@ -17,11 +11,6 @@ module.exports.renderInventory = (req, res) => {
     ]
   })
     .then(products => {
-      console.log('products', products);
       res.status(200).render('inventory', { products });
     });
-};
-
-const getProducts = (id) => {
-
 };
