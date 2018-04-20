de37fb4f9b7679d1a5336e88d57a2f19d734b811
Trey Mosley <iiimosley@gmail.com>
2018-04-20 13:05:18 -0500

    fetch one product type with products

--- controllers/productTypesCtrl.js
+++ controllers/productTypesCtrl.js
@@ -1,13 +1,25 @@
 'use strict'
 
+
 module.exports.renderAllProductTypes = (req, res, next) => {
     const { ProductType, Product } = req.app.get('models');
     ProductType.findAll({
-        include: [{ model: Product }]
+        include: [{ 
+            model: Product, 
+        }]
     })
     .then(prodtypes=> res.status(200).json(prodtypes));
 }
 
 module.exports.renderOneProductType = (req, res, next) => {
-
+    const { ProductType, Product } = req.app.get('models');
+    ProductType.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Product,
+        }]
+    })
+    .then(prodtypes => res.status(200).json(prodtypes));
 }
