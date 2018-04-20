1940bb5a37559eb277cc253c4a94a825fb1490bc
Trey Mosley <iiimosley@gmail.com>
2018-04-20 15:06:01 -0500

    return only 3 prods per type (all prod types)

--- controllers/productTypesCtrl.js
+++ controllers/productTypesCtrl.js
@@ -1,25 +1,25 @@
 'use strict'
 
-
 module.exports.renderAllProductTypes = (req, res, next) => {
     const { ProductType, Product } = req.app.get('models');
     ProductType.findAll({
-        include: [{ 
-            model: Product, 
-        }]
+        include: [{ model: Product }]
     })
-    .then(prodtypes=> res.status(200).json(prodtypes));
+    .then(prodtypes=> {
+        let newProdTypes = prodtypes.map(({Products})=> {
+            const [a, b, c] = Products;
+            Products = [a,b,c];
+            return { Products };
+        });
+        res.status(200).json(newProdTypes);
+    });
 }
 
 module.exports.renderOneProductType = (req, res, next) => {
     const { ProductType, Product } = req.app.get('models');
     ProductType.findOne({
-        where: {
-            id: req.params.id
-        },
-        include: [{
-            model: Product,
-        }]
+        where: { id: req.params.id },
+        include: [{ model: Product }]
     })
     .then(prodtypes => res.status(200).json(prodtypes));
 }
