e4b5390c001c4e8e8feb32ffd10b36baa7ef5927
Trey Mosley <iiimosley@gmail.com>
2018-04-22 17:52:21 -0500

    /categories only renders 3 prods per type

--- controllers/productTypesCtrl.js
+++ controllers/productTypesCtrl.js
@@ -6,12 +6,12 @@ module.exports.renderAllProductTypes = (req, res, next) => {
         include: [{ model: Product }]
     })
     .then(prodtypes=> {
-        let newProdTypes = prodtypes.map(({Products})=> {
-            const [a, b, c] = Products;
-            Products = [a,b,c];
-            return { Products };
-        });
-        res.status(200).json(newProdTypes);
+        let allTypes = JSON.parse(JSON.stringify(prodtypes));
+        for(let i=0;i<allTypes.length;i++){
+            allTypes[i].Products = allTypes[i].Products.slice(0, 3);
+        }
+        res.status(200).json(allTypes);
+        // res.render('categories', {allProdTypes});
     });
 }
 
@@ -23,3 +23,10 @@ module.exports.renderOneProductType = (req, res, next) => {
     })
     .then(prodtypes => res.status(200).json(prodtypes));
 }
+
+        // let allProdTypes = prodtypes.map(({Products})=> {
+        //     const [a, b, c] = Products;
+        //     Products = [a,b,c];
+        //     return { Products };
+        // });
+        // prodtypes.Products = prodtypes.Products.slice(0,3);
