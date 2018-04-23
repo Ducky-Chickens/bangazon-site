985d9de60a2888fda59c558e1f2ffd48214ce8f0
Trey Mosley <iiimosley@gmail.com>
2018-04-22 19:39:40 -0500

    javascript template running on index template

--- client/client.js
+++ client/client.js
@@ -1,22 +1,17 @@
-console.log('Is this thing on?');
+// document.getElementById("showLogin").addEventListener('click', () => {
+//   console.log('Wow, that worked!', location.origin);
+//   location.href = `${location.origin}/login`;
+// });
 
-document.getElementById("showLogin").addEventListener('click', () => {
-  console.log('Wow, that worked!', location.origin);
-  location.href = `${location.origin}/login`;
-});
-
-document.getElementById("showRegister").addEventListener('click', () => {
-  location.href = `${location.origin}/register`;
-});
+// document.getElementById("showRegister").addEventListener('click', () => {
+//   location.href = `${location.origin}/register`;
+// });
 
 let allTypes = document.getElementsByClassName('productType');
 
-console.log('hello!');
-
-// .forEach(prod=>{
-//   console.log(prod);
-//   // prod.addEventListener('mousedown', (e)=>{
-//   //   console.log(e.target);
-//   // })
-//   // prod.addEventListener('mouseup')
-// });
+for (let i = 0; i < allTypes.length; i++) {
+  allTypes[i].addEventListener('mousedown', (e)=>{
+    console.log(e.currentTarget);
+  });
+  // prod.addEventListener('mouseup')
+}
