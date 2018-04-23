78399d16eb39c99428034c5438470fb29b3cbd30
Trey Mosley <iiimosley@gmail.com>
2018-04-23 11:54:39 -0500

    add product form rendered to .../inventory/add

--- controllers/inventoryCtrl.js
+++ controllers/inventoryCtrl.js
@@ -14,3 +14,18 @@ module.exports.renderInventory = (req, res) => {
       res.status(200).render('inventory', { products });
     });
 };
+
+module.exports.renderAddInventory = (req, res) => {
+  const { Product, OrderProduct } = req.app.get('models');
+  Product.findAll({
+    where: { user_id: req.user.id },
+    include: [
+      {
+        model: OrderProduct
+      }
+    ]
+  })
+    .then(products => {
+      res.status(200).render('inventory', { products });
+    });
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,5 +5,6 @@ const router = Router();
 const { renderInventory } = require('../controllers/inventoryCtrl');
 
 router.get('/inventory', renderInventory);
+router.get('/inventory/add', (req,res,next)=>res.render('product-form'))
 
 module.exports = router;
