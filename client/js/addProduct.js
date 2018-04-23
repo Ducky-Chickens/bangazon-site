f09fb53303afb4cedd7e76677028db338a6f977c
Trey Mosley <iiimosley@gmail.com>
2018-04-23 18:26:41 -0500

    fetch constructed and posting to API

--- client/js/addProduct.js
+++ client/js/addProduct.js
@@ -1,9 +1,16 @@
 
-let errAst = Array.from(document.querySelectorAll('.errAstrik'));
+let errAst = Array.from(document.querySelectorAll('.errAsterisk'));
 
 const formComplete = (cv) => cv.value;
 
-const postForm = () => console.log('hell yea!');
+const postForm = (allInputs) => {
+    let newProduct = {};
+    allInputs.forEach(line=> newProduct[line.getAttribute('prop')] = line.value);
+    fetch(`${location.href}`, {
+        method: 'POST',
+        body: JSON.stringify(newProduct)
+    })
+};
 
 const throwErrs = (allInputs) => allInputs.forEach(line=>{
     if (!line.value) line.nextSibling.innerText = "*";
@@ -12,5 +19,5 @@ const throwErrs = (allInputs) => allInputs.forEach(line=>{
 document.getElementById('addProductButton').addEventListener('mouseup', ()=>{
     let allInputs = Array.from(document.querySelectorAll('#productForm input, #typeSelect'));
     errAst.forEach(ast=>ast.innerText = '');
-    allInputs.every(formComplete) ? postForm() : throwErrs(allInputs); 
+    allInputs.every(formComplete) ? postForm(allInputs) : throwErrs(allInputs); 
 });
