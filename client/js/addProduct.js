df3b59c54c0cfa1d1e7ddd0da9109ce6db7c6fb8
Trey Mosley <iiimosley@gmail.com>
2018-04-23 15:36:56 -0500

    signify if content is needed

--- client/js/addProduct.js
+++ client/js/addProduct.js
@@ -1,9 +1,17 @@
 
+// let typeSelect = document.getElementById('typeSelect');
+let errAst = document.querySelectorAll('.errAstrik');
+
 document.getElementById('addProductButton').addEventListener('mouseup', ()=>{
-    let allInputs = document.querySelectorAll('#productForm input');
+    for (let i = 0; i < errAst.length; i++) {
+        errAst[i].innerText = '';
+    }
+    let allInputs = document.querySelectorAll('#productForm input, #typeSelect');
     for (let i = 0; i < allInputs.length; i++) {
         if (allInputs[i].value){
-            console.log(allInputs[i].prop, allInputs[i].value);
+            console.log('yep', allInputs[i].getAttribute('prop'), allInputs[i].value);
+        } else {
+            allInputs[i].nextSibling.innerText = "*";
         }
     }
 });
