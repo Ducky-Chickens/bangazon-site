7a399a4f3278ac1a3777cbb18b97c52a57ecbf6f
Hunter Phillips <hkphillips42@gmail.com>
2018-04-24 13:49:31 -0500

    merge

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,20 +2,12 @@
 const { Router } = require('express');
 const router = Router();
 
-<<<<<<< HEAD
-const { renderInventory, removeInventoryProduct, renderAddInventory, addInventory } = require('../controllers/inventoryCtrl');
-=======
-const { gatherInventory, renderInventory, unArrayCount, findMatch, renderAddInventory, addInventory } = require('../controllers/inventoryCtrl');
->>>>>>> master
+const { gatherInventory, renderInventory, unArrayCount, findMatch, renderAddInventory, addInventory, removeInventoryProduct } = require('../controllers/inventoryCtrl');
 
 router.get('/inventory', gatherInventory, unArrayCount, findMatch, renderInventory);
 router.get('/inventory/add', renderAddInventory);
-<<<<<<< HEAD
-router.post('/inventory/add', addInventory, renderInventory);
-router.delete('/inventory', removeInventoryProduct)
-=======
 router.post('/inventory/add', addInventory, gatherInventory, unArrayCount, findMatch, renderInventory);
->>>>>>> master
+router.delete('/inventory', removeInventoryProduct)
 
 module.exports = router;
 
