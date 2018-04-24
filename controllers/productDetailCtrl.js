d69e1831ee6d0b23830b76c594b9c30a2514da5b
joechesney <33613119+joechesney@users.noreply.github.com>
2018-04-24 13:55:53 -0500

    almost got it working. need to find the right name for the orderProduct model

--- controllers/productDetailCtrl.js
+++ controllers/productDetailCtrl.js
@@ -11,3 +11,40 @@ module.exports.renderProductDetails = (req, res) => {
   })
 };  
 
+module.exports.addProductToCart = (req, res) => {
+  req.app.get("models").Order.find({where: {user_id: req.session.passport.user.id, payment_type_id: null}})
+  .then(order=>{
+    // if there isnt one, create one and post it to the orders table
+    if(!order){
+      let order = {
+        user_id: req.session.passport.user.id,
+        payment_type_id: null,
+      }
+      req.app.get("models").Order.create(order)
+      .then(newOrder=>{
+        console.log('order',newOrder);
+        // post new order_product here
+      })
+      //post new order to the orders table here
+
+    } else if(order){
+      
+      let product = {
+        product_id: +req.body.product_id,
+        order_id: order.dataValues.id,
+      }
+      req.app.get("models").order_product.create(product)
+      .then(newProduct=>{
+        console.log('newProduct',newProduct);
+        // post new order_product here
+      })
+      console.log("product to add:", product  );
+      //post this order_product to the order_product table
+
+    } else {
+      console.log('WTF MAN');
+    }
+  })
+
+};  
+
