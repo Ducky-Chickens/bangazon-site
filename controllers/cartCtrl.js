2a8a3e48b0e49c58916c9a463a9aca08521f7fdc
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-04-25 14:26:05 -0500

    orders can be completed

--- controllers/cartCtrl.js
+++ controllers/cartCtrl.js
@@ -14,7 +14,37 @@ module.exports.renderOrderProducts = (req, res, next) => {
         for (let i = 0; i < products.length; i++) {
           orderTotal = orderTotal + parseInt(products[i].price);
         }
-        res.render('cart', { products, orderTotal });
+        res.render('cart', { products, orderTotal, order });
       });
     });
+};
+
+module.exports.renderCheckoutPayments = (req, res, next) => {
+  req.app.get('models').
+  PaymentType.findAll({
+    where: { user_id: req.user.id, active: 't', },
+  }).then(paymentTypes => {
+    res.render("checkout", {
+      ...req.user,
+      paymentTypes,
+    });
+  });
+}
+
+module.exports.checkOut = (req, res, next) => {
+  req.app.get('models').Order.find({
+    where: {user_id: req.user.id, payment_type_id: { [Op.eq]: null }}
+  }).then(order => {
+    req.app.get('models').Order.update(
+      { payment_type_id: req.body.payType },
+      {
+        where: {
+          id: order.id
+        }
+      }
+    ).then(() => {
+      res.redirect('/cart/complete');
+      res.render('complete');
+    });
+  });
 };
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -2,8 +2,11 @@
 
 const { Router } = require('express');
 const router = Router();
-const { renderOrderProducts } = require('../controllers/cartCtrl');
+const { renderOrderProducts, renderCheckoutPayments, checkOut } = require('../controllers/cartCtrl');
 
 router.get('/cart', renderOrderProducts);
+router.get('/cart/checkout/:id', renderCheckoutPayments);
+router.patch('/cart/checkout', checkOut)
+router.get('/cart/complete', (req, res) => res.render('complete'))
 
 module.exports = router;
