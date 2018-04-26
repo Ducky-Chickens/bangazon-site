f4e73a4d472cd7e41f4c779a2092f5317a25bb54
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-04-26 09:31:12 -0500

    removed excess cart ctrl code

--- controllers/cartCtrl.js
+++ controllers/cartCtrl.js
@@ -9,16 +9,6 @@ module.exports.renderOrderProducts = (req, res, next) => {
       where: { user_id: req.user.id, payment_type_id: { [Op.eq]: null } }
     })
     .then(order => {
-      if (order === null) {
-        res.render('cart');
-      }
-      order.getProducts().then(products => {
-        let orderTotal = 0;
-        for (let i = 0; i < products.length; i++) {
-          orderTotal = orderTotal + parseInt(products[i].price);
-        }
-        res.render('cart', { products, orderTotal, order });
-      });
       if (order) {
         order.getProducts().then(products => {
           let orderTotal = 0;
@@ -28,7 +18,7 @@ module.exports.renderOrderProducts = (req, res, next) => {
           res.render('cart', { products, orderTotal, order });
         });
       } else {
-        res.render('cart', {});
+        res.render('cart');
       }
     });
 };
